49d50c06221bac26467749c4ece3b2e1ddb809d1
izzy <me@insrt.uk>
2025-10-24 13:14:48 +0100

    feat: add support for changing track gain

--- src/components/participant/AudioTrack.tsx
+++ src/components/participant/AudioTrack.tsx
@@ -7,7 +7,8 @@ import { log } from '@livekit/components-core'
 import { RemoteAudioTrack, RemoteTrackPublication } from 'livekit-client'
 import { useEnsureTrackRef } from '../../context'
 import type { JSX } from 'solid-js/jsx-runtime'
-import { createEffect } from 'solid-js'
+import { createEffect, onCleanup } from 'solid-js'
+import { useMediaDeviceSelect } from 'src/signals'
 
 /** @public */
 export interface AudioTrackProps extends JSX.AudioHTMLAttributes<HTMLAudioElement> {
@@ -24,11 +25,18 @@ export interface AudioTrackProps extends JSX.AudioHTMLAttributes<HTMLAudioElemen
    * @alpha
    */
   muted?: boolean
+  /**
+   * Whether to enable gain node when volume > 1
+   */
+  enableBoosting?: boolean
 }
 
 /**
  * The AudioTrack component is responsible for rendering participant audio tracks.
  * This component must have access to the participant's context, or alternatively pass it a `Participant` as a property.
+ *
+ * Must be using key in parent tree: (e.g. <Key />, <Show keyed />)
+ * (to ensure AudioTrack is unique to each Track and props don't suddenly update to another track!)
  *
  * @example
  * ```tsx
@@ -57,13 +65,60 @@ export function AudioTrack(props: AudioTrackProps) {
     props.onSubscriptionStatusChanged?.(!!isSubscribed())
   })
 
+  let gainContext: { gainNode: GainNode; audioContext: AudioContext } | undefined
+  const { activeDeviceId } = useMediaDeviceSelect({ kind: 'audiooutput' })
+
+  onCleanup(() => {
+    if (gainContext) {
+      gainContext.gainNode.disconnect()
+      gainContext.audioContext.close()
+    }
+  })
+
   createEffect(() => {
     const t = track()
     if (t === undefined || props.volume === undefined) {
       return
     }
     if (t instanceof RemoteAudioTrack) {
-      t.setVolume(props.volume)
+      if (!props.enableBoosting || props.volume <= 1) {
+        t.setVolume(props.volume)
+
+        if (gainContext) {
+          gainContext.gainNode.disconnect()
+          gainContext.audioContext.close()
+          gainContext = undefined
+        }
+      } else {
+        if (gainContext) {
+          gainContext.gainNode.gain.value = props.volume
+        } else {
+          t.setVolume(0)
+
+          const audioContext = new AudioContext()
+
+          if ('setSinkId' in AudioContext.prototype) {
+            ;(audioContext as never as { setSinkId: (sinkId: string) => Promise<void> }).setSinkId(
+              activeDeviceId(),
+            )
+          } else {
+            console.error(`Browser does not support AudioContext#setSyncId!`)
+          }
+
+          const gainNode = audioContext.createGain()
+
+          const source = audioContext.createMediaStreamSource(new MediaStream([t.mediaStreamTrack]))
+
+          gainNode.gain.value = props.volume
+          gainNode.connect(audioContext.destination)
+          source.connect(gainNode)
+
+          gainContext = {
+            audioContext,
+            gainNode,
+          }
+        }
+      }
     } else {
       log.warn('Volume can only be set on remote audio tracks.')
     }
